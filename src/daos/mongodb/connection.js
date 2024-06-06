260e6fcdd2fe71165c2f9ba2f4edc6a81af80d09
fgalegria <francisco.gomez@comsys.mx>
2024-06-05 19:38:37 -0700

    Troubleshooting to run on production

--- src/daos/mongodb/connection.js
+++ src/daos/mongodb/connection.js
@@ -1,12 +1,21 @@
 import dotenv from "dotenv";
 import { connect } from "mongoose";
+
 dotenv.config();
 
 const connectionString = process.env.DB_CONNECTION_STRING;
 
-try {
-    await connect(connectionString);
-    console.log('Conectado a la db');
-} catch (error) {
-    console.log(error)
-}
+const connectToDatabase = async () => {
+    try {
+        await connect(connectionString, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('Connected to the database');
+    } catch (error) {
+        console.error('Error connecting to the database', error);
+        process.exit(1);
+    }
+};
+
+connectToDatabase();
--- src/server.js
+++ src/server.js
@@ -2,16 +2,22 @@ import express from "express";
 import productsRouter from "./routes/products.router.js";
 import cartsRouter from "./routes/carts.router.js";
 import "./daos/mongodb/connection.js";
+import "dotenv/config"
 
 const app = express();
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/products', productsRouter);
 app.use('/carts', cartsRouter);
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`)
