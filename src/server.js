5a1fba27a4276770886a7929040e7f0b5f3e9d7f
fgalegria <francisco.gomez@comsys.mx>
2024-06-05 19:43:09 -0700

    Final changes for production

--- src/server.js
+++ src/server.js
@@ -14,11 +14,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/products', productsRouter);
 app.use('/carts', cartsRouter);
 
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something went wrong!');
-});
-
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`)
 });
