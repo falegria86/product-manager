09ae35749d2687a823d9fead0a50f06f93c8435a
fgalegria <francisco.gomez@comsys.mx>
2024-06-06 12:24:15 -0700

    Added cors management

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import productsRouter from "./routes/products.router.js";
 import cartsRouter from "./routes/carts.router.js";
 import "./daos/mongodb/connection.js";
@@ -7,6 +8,8 @@ import "dotenv/config"
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
