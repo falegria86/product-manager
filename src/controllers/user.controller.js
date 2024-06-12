385b612b093a4f4e2917bf38bfe6178251647bd3
fgalegria <francisco.gomez@comsys.mx>
2024-06-12 16:02:22 -0700

    Se agregó info de session

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -8,11 +8,11 @@ export const login = async (req, res) => {
         const { email, password } = req.body;
         const user = await userDao.login(email, password);
         if (!user) res.status(401).json({ message: "No estás autorizado" });
-        //res.redirect('/error-login)
         else {
-            req.session.email = email;
-            req.session.password = password;
-            // res.redirect("/views/profile");
+            req.session.info = {
+                loggedIn: true,
+                email,
+            }
             res.status(200).json({ message: 'Welcome!' })
         }
     } catch (error) {
@@ -29,15 +29,13 @@ export const register = async (req, res) => {
                 ...req.body,
                 role: "admin",
             });
-            if (!user) res.status(401).json({ message: "User doesn't exist!" });
-            // else res.redirect("/login");
+            if (!user) res.status(401).json({ message: "Error when registering user!" });
             else res.status(200).json({ message: 'User registered correctly!' })
         }
 
         const user = await userDao.register(req.body);
-        if (!user) res.status(401).json({ message: "User doesn't exist!" });
+        if (!user) res.status(401).json({ message: "Error when registering user!" });
 
-        // else res.redirect("/login");
         else res.status(200).json({ message: 'User registered correctly!' })
     } catch (error) {
         throw new Error(error);
@@ -54,5 +52,5 @@ export const infoSession = (req, res) => {
 
 export const logout = (req, res) => {
     req.session.destroy();
-    res.send("session destroy");
+    res.send("Logged out correctly!");
 };
